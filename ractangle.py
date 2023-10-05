13f1b111d81db6651012a88ccc4e6e98580666cd
[simon] <[shulgayury92@gmail.com]>
2023-10-05 21:08:22 +0300

    змінні в середині рядків

--- ractangle.py
+++ ractangle.py
@@ -5,14 +5,14 @@ tk = Tk()
 canvas = Canvas(tk, width=700, height=700)
 canvas.pack()
 
-colors = ('green', 'red', 'blue', 'orange', 'yellow', 'pink', 'purple', 'violet', 'magenta', 'cyan')
-def random_rectangle(width, height, colors):
+colors = ('green', 'red', 'blue', 'orange', 'yellow', 'pink', 'purple', 'violet', 'magenta', 'cyan', '#ffd800')
+def random_rectangle(width,height,colors):
     x1 = random.randrange(width)
     y1 = random.randrange(height)
     x2 = x1 + random.randrange(width)
     y2 = y1 + random.randrange(height)
     color_choice = random.choice(colors)
-    canvas.create_rectangle(x1, y1, x2, y2, fill= color_choice)
+    canvas.create_rectangle(x1, y1, x2, y2, fill=color_choice)
 
 
 for x in range(0, 100):
@@ -20,8 +20,5 @@ for x in range(0, 100):
 
 Tk.mainloop(canvas)
 
-print(abs(-1000))
-a = 1000
 
-
-
+print('%02x' % 15)
