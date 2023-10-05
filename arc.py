0b0a9954d2b0758f23706244d2a9b83970191515
[simon] <[shulgayury92@gmail.com]>
2023-10-05 22:31:48 +0300

    додається arc.py

---
+++ arc.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import random
+
+tk = Tk()
+canvas = Canvas(tk, width=700, height=700)
+canvas.pack()
+
+canvas.create_arc(10, 10, 200, 100, extent=180, style=ARC)
+
+
+
+Tk.mainloop(canvas)
