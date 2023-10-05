b97e331be76a3f577c812a6552dabe99de06bdd7
[simon] <[shulgayury92@gmail.com]>
2023-10-05 15:28:18 +0300

    colored rectangles01

--- ractangle.py
+++ ractangle.py
@@ -2,19 +2,23 @@ from tkinter import *
 import random
 
 tk = Tk()
-canvas = Canvas(tk, width=400, height=400)
+canvas = Canvas(tk, width=700, height=700)
 canvas.pack()
 
-
-def random_rectangle(width, height):
+colors = ('green', 'red', 'blue', 'orange', 'yellow', 'pink', 'purple', 'violet', 'magenta', 'cyan')
+def random_rectangle(width, height, colors):
     x1 = random.randrange(width)
     y1 = random.randrange(height)
     x2 = x1 + random.randrange(width)
     y2 = y1 + random.randrange(height)
-    canvas.create_rectangle(x1, y1, x2, y2)
+    color_choice = random.choice(colors)
+    canvas.create_rectangle(x1, y1, x2, y2, fill= color_choice)
 
 
 for x in range(0, 100):
-    random_rectangle(400, 400)
+    random_rectangle(400, 400, colors)
 
 Tk.mainloop(canvas)
+
+
+
