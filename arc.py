414c80f1472362ed03093e2da5c2644fdec720fa
[simon] <[shulgayury92@gmail.com]>
2023-10-05 23:34:49 +0300

    1: erc малює задану дугу

--- arc.py
+++ arc.py
@@ -5,7 +5,9 @@ tk = Tk()
 canvas = Canvas(tk, width=700, height=700)
 canvas.pack()
 
-canvas.create_arc(10, 10, 200, 100, extent=180, style=ARC)
+canvas.create_arc(10, 10, 200, 100, extent=45, style=ARC)
+
+
 
 
 
